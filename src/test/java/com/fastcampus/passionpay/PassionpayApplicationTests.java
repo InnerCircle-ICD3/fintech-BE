c4073c74ed8000b644f9542578bfcd53aa00b5fb
Acacian <koo9811@gmail.com>
2025-04-27 15:05:49 +0900

    Feat : Spring Boot Project Init

---
+++ src/test/java/com/fastcampus/passionpay/PassionpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.passionpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassionpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
