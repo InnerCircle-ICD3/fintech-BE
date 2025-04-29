1804d279ad56b82c1783bcfcf68ae822e5cba7c9
Acacian <koo9811@gmail.com>
2025-04-29 20:29:43 +0900

    Feat : Module Setup

---
+++ payment-core/src/test/java/com/fastcampus/paymentcore/PaymentCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.paymentcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-infra/src/test/java/com/fastcampus/paymentinfra/PaymentInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.paymentinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentInfraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
