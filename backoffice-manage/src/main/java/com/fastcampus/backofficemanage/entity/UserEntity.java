a862c941574129a2d2eca06762c214ec3af52564
Acacian <koo9811@gmail.com>
2025-04-29 21:31:37 +0900

    Feat : User

---
+++ backoffice-manage/src/main/java/com/fastcampus/backofficemanage/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.fastcampus.backofficemanage.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
