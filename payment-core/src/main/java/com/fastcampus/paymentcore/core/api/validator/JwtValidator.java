75f2cf333d3be6220a707061c1d03d7f11e18aaa
parksay <hellobigworld.parksay@gmail.com>
2025-05-07 22:03:42 +0900

    empty classes

---
+++ payment-core/src/main/java/com/fastcampus/paymentcore/core/api/validator/JwtValidator.java
@@ -0,0 +1,4 @@
+package com.fastcampus.paymentcore.core.api.validator;
+
+public class JwtValidator {
+}
---
+++ payment-core/src/main/java/com/fastcampus/paymentcore/core/common/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.fastcampus.paymentcore.core.common.config;
+
+public class SecurityConfig {
+}
