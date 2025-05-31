4ea25a30a80361928a68736eeacc172e8b637bb3
Acacian <koo9811@gmail.com>
2025-05-31 11:52:03 +0900

    Feat : Entity에 Cardinfo추가 / 결제 빌드 시 생기던 오류 수정

--- payment-infra/src/main/java/com/fastcampus/paymentinfra/repository/KeysRepository.java
+++ payment-infra/src/main/java/com/fastcampus/paymentinfra/repository/KeysRepository.java
@@ -1,11 +1,11 @@
 package com.fastcampus.paymentinfra.repository;
 
-import com.fastcampus.paymentinfra.entity.Keys;
+import com.fastcampus.paymentinfra.entity.KeysReadOnly;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface KeysRepository extends JpaRepository<Keys, Long> {
+public interface KeysRepository extends JpaRepository<KeysReadOnly, Long> {
     // merchantId 기준으로 조회
-    Optional<Keys> findByMerchantId(Long merchantId);
+    Optional<KeysReadOnly> findByMerchantId(Long merchantId);
 }
--- payment-infra/src/main/java/com/fastcampus/paymentinfra/repository/PaymentRepository.java
+++ payment-infra/src/main/java/com/fastcampus/paymentinfra/repository/PaymentRepository.java
@@ -2,6 +2,7 @@ package com.fastcampus.paymentinfra.repository;
 
 import com.fastcampus.paymentinfra.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByTransactionId(Long transactionId);
 
     List<Payment> findByPaymentStatus(String paymentStatus);
+
+    @Query("SELECT p FROM Payment p JOIN FETCH p.cardInfo WHERE p.userId = :userId")
+    List<Payment> findByUserIdWithCardInfo(Long userId);
 }
