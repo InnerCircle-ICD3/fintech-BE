9910f9873715636259fa86a03bc28236d96c8005
Acacian <koo9811@gmail.com>
2025-06-04 01:32:40 +0900

    Committest

--- backoffice-manage/src/main/java/com/fastcampus/backofficemanage/dto/login/Login.java
+++ backoffice-manage/src/main/java/com/fastcampus/backofficemanage/dto/login/Login.java
@@ -21,7 +21,7 @@ public abstract class Login {
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(max = 20, message = "비밀번호는 최대 20자까지 입력 가능합니다.")
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).{8,20}$",
+            regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).{8,20}$",
             message = "비밀번호는 특수문자, 영어 소문자, 숫자를 각각 최소 하나씩 포함해야 하며 8자 이상 20자 이하여야 합니다."
     )
     @Schema(description = "로그인 비밀번호", example = "passw0rd!")
