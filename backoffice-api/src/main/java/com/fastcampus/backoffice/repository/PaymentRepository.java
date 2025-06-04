05427f63a6ab96c234abb57dd7be530c84ff2483
parksay <hellobigworld.parksay@gmail.com>
2025-06-04 23:58:15 +0900

    backoffice changes by payment

--- backoffice-api/src/main/java/com/fastcampus/backoffice/repository/PaymentRepository.java
+++ backoffice-api/src/main/java/com/fastcampus/backoffice/repository/PaymentRepository.java
@@ -1,6 +1,6 @@
 package com.fastcampus.backoffice.repository;
 
-import com.fastcampus.paymentinfra.entity.Payment;
+import com.fastcampus.payment.entity.Payment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
