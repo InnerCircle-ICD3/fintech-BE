f9e9941e33351062668bf7704d9730f61e6c4dbc
Radies <ranyoung0102@gmail.com>
2025-06-07 23:22:58 +0900

    Paymentmethod- import 변경

--- payment/src/main/java/com/fastcampus/payment/repository/PaymentMethodRepository.java
+++ payment/src/main/java/com/fastcampus/payment/repository/PaymentMethodRepository.java
@@ -3,10 +3,11 @@ package com.fastcampus.payment.repository;
 
 import com.fastcampus.payment.entity.PaymentMethod;
 import com.fastcampus.payment.entity.PaymentMethodType;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 import java.util.Optional;
