cc09027772f43b67468e7fd0ea1e0fae3043ec42
parksay <hellobigworld.parksay@gmail.com>
2025-06-07 11:05:55 +0900

    cors exception

--- payment/src/main/java/com/fastcampus/payment/config/RedisTransactionTemplateConfig.java
+++ payment/src/main/java/com/fastcampus/payment/config/RedisTransactionTemplateConfig.java
@@ -1,6 +1,8 @@
 package com.fastcampus.payment.config;
 
 import com.fastcampus.payment.entity.Transaction;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,6 +11,8 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@ConditionalOnClass(RedisConnectionFactory.class)
+@ConditionalOnProperty(name = "spring.redis.host", matchIfMissing = false)
 public class RedisTransactionTemplateConfig {
 
     @Bean
--- payment/src/test/java/com/fastcampus/payment/PaymentApplicationTests.java
+++ payment/src/test/java/com/fastcampus/payment/PaymentApplicationTests.java
@@ -2,8 +2,10 @@ package com.fastcampus.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = PaymentApplication.class)
+@ActiveProfiles("test")
 class PaymentApplicationTests {
 
     @Test
