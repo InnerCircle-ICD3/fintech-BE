4d93e29b61206c94c01cf226ab0ff5dfb726ae0e
parksay <hellobigworld.parksay@gmail.com>
2025-06-14 14:21:43 +0900

    test yml

--- payment/src/test/java/com/fastcampus/payment/parksay/ParksayTest.java
+++ payment/src/test/java/com/fastcampus/payment/parksay/ParksayTest.java
@@ -15,12 +15,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.UUID;
 
 @SpringBootTest(classes = PaymentApplication.class)
+@ActiveProfiles("test")
 public class ParksayTest {
 
     @Autowired
